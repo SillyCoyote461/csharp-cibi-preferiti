98cd23a172288c92627e4d927957b73bf201b0df
Francesco C <fran461j@outlook.it>
2023-03-15 16:38:24 +0100

    Aggiungere i file di progetto.

---
+++ CibiPreferiti/Program.cs
@@ -0,0 +1,32 @@
+﻿//cibi
+string[] favoriteFoods = {"Pasta", "Cioccolato", "Carne", "Kebab", "Hamburger", "Verdura"};
+//totale cibi + classifica
+Console.WriteLine("Totale cibi: " + favoriteFoods.Length);
+Console.WriteLine("Classifica cibi");
+{
+        int pos = 0;
+    for(int i = 0; i < favoriteFoods.Length; i++)
+    {
+        pos++;
+        Console.WriteLine(pos + " posizione: " +favoriteFoods[i]);
+    }
+}
+//migliore e peggiore cibo
+Console.WriteLine("Cibo migliore: " + favoriteFoods[0]);
+Console.WriteLine("Peggiore miglior cibo: " + favoriteFoods[4]);
+
+//cibo metá classifica
+int middle = favoriteFoods.Length / 2;
+{
+    int x = favoriteFoods.Length % 2;
+    if(x != 0)
+    {
+        Console.WriteLine("Cibo di metá classifica: " + favoriteFoods[middle]);
+    }
+    else
+    {
+        Console.WriteLine("Cibi di metá classifica: " + favoriteFoods[middle - 1] + ", " + favoriteFoods[middle]);
+    }
+}
+
+
